8152817291972ce499d8387c5b191f6547d58692
bermac16 <bermac16@htl-kaindorf.at>
2019-03-22 12:27:51 +0100

    create password class

---
+++ PasswordProducerConsumer/src/main/java/htl/bergmann/passwordproducerconsumer/Password.java
@@ -0,0 +1,14 @@
+
+package htl.bergmann.passwordproducerconsumer;
+
+public class Password {
+    private String password;
+
+    public Password(String password) {
+        this.password = password;
+    }
+    
+    public boolean check(String password) {
+        return this.password == password;
+    }
+}
